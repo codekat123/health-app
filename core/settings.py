e0997b3bd84b080b89c99e4e02877ca903b6b853
Ahmed Gaber <ahmed.gaber4371@gmail.com>
2025-08-20 23:13:06 +0300

    update

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "fulfilling-creativity-production.up.railway.app",
+    "127.0.0.1",
+    "localhost",
+]
+
 
 
 # Application definition
